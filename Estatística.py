621087a1a1f9ce4e58900d2896f3b541417e58cf
Carple <68165466+Calitts@users.noreply.github.com>
2021-09-21 23:10:36 -0300

    Added __getitem__ and __repr__ functions and ajusted the comments

--- Estatística.py
+++ Estatística.py
@@ -1,15 +1,19 @@
 import random
 
-class Calculate:  # The main class, reponsible for calculating and printing the values
-    def __init__(self, list=[1, 1, 1, 3, 4, 4, 2, 2, 2], pre_cal=True):
-        self.list_list = list
+class Calculate:  
+    """ The main class, reponsible for calculating and printing the values"""
+
+    def __init__(self, listed=[1, 1, 1, 3, 4, 4, 2, 2, 2], pre_cal=True) -> None:
+        self.list_list = listed
         self.aprx = 2
         self.final = {}
         self.candidate = []
         if pre_cal:  # If the program will calculate altomicaly the values
             self.run()
 
-    def run(self, transform=True, show=False):  # Calculates the value and puts in a dict (self.final)
+    def run(self, transform=True, show=False):  
+        """Calculates the value and puts in a dict (self.final)"""
+
         acu = []
         fra = []
 
@@ -43,11 +47,23 @@ class Calculate:  # The main class, reponsible for calculating and printing the 
         
         if show:
             self.show()
+        
+    def __getitem__(self, index: int):
+        """ Give the correct value of index provided"""
+        return self.final[index]
+    
+    def __repr__(self) -> str:
+        """ Give the value in a string format"""
+        return str(self.final)
 
-    def give(self):  # Giev the value of the final in form of a dict
+
+    def give(self) -> dict:  
+        """ Give the value of the final in a dict format"""
         return self.final
 
-    def transform(self):  # Round the values to sum up to 100%
+    def transform(self):  
+        """ Round the values to sum up to 100%""""
+
         perc = self.final["Last"]["Fra"]
         while perc != 100.0:
             choice = random.choice(self.candidate)
@@ -63,7 +79,9 @@ class Calculate:  # The main class, reponsible for calculating and printing the 
                 perc = round(perc + 0.01, self.aprx)
                 self.update()
 
-    def update(self):  # Update the value of the final dict
+    def update(self):  
+        """Update the value of the final dict"""
+        
         fra = []
         for index in self.final.keys():
             fra.append(float(self.final[index]["Fr"]))
@@ -74,7 +92,8 @@ class Calculate:  # The main class, reponsible for calculating and printing the 
         # print(round(sum(fra), self.aprx-1)) - Debug
         # print(self.final["Last"]["Fra"]) - Debug
 
-    def show(self):  # Print the value of the final dict
+    def show(self):  
+        """ Print the value of the final dict"""
         print(self.final)
 
 
